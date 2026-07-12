7d13ceef8fba3a76196f948e18724c64857aacc2
kahtaf <kahtaf@gmail.com>
2026-07-12 16:47:41 -0400

    chore: scaffold worker and site toolchain

---
+++ astro.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config'
+
+export default defineConfig({
+  srcDir: './site',
+  publicDir: './public',
+  outDir: './dist',
+  output: 'static',
+  build: {
+    inlineStylesheets: 'auto',
+  },
+})
